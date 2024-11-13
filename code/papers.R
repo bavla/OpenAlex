c0ba1375e73693d591e14cd9e3c3c11a1e0134bd
bavla <vladimir.batagelj@gmail.com>
2024-11-13 05:39:11 +0100

    Update papers.R

--- code/papers.R
+++ code/papers.R
@@ -1,17 +1,13 @@
-
-> wdir <- "C:/Users/vlado/docs/papers/2024/Sreda/1353"
-> setwd(wdir)
+> # number of papers in OpenAlex in each year ymin:ymax
+> ymin <- 1980; ymax <- 2024 
 > library(httr)
 > library(jsonlite)
-
-> URL <- "https://api.openalex.org/works?filter=publication_year:1990-2024&group-by=publication_year"
+> works <- "https://api.openalex.org/works"
+> URL <- paste0(works,"?filter=publication_year:",
++    ymin,"-",ymax,"&group-by=publication_year")
 > res <- GET(URL)
-> # str(res)
-> # res$date
 > cont <- fromJSON(rawToChar(res$content))
-> # names(cont)
-> # str(cont)
 > year <- as.integer(cont$group_by$key)
 > nPapers <- cont$group_by$count
 > plot(year,nPapers,main="OpenAlex",pch=16)
-> plot(year,log(nPapers),main="OpenAlex",pch=16)
+> # plot(year,log(nPapers),main="OpenAlex",pch=16)
