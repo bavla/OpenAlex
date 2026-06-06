874c58bc408c11637d965eec588b77942309faed
bavla <vladimir.batagelj@gmail.com>
2026-06-06 23:39:13 +0200

    Update OpenAlex2Pajek.R

--- code/OpenAlex2Pajek.R
+++ code/OpenAlex2Pajek.R
@@ -20,10 +20,12 @@
 # version 7. July 27, 2025; OpenAlexAuthors
 # version 8. August 15, 2025; removehash, authorsId2name, worksId2name
 # version 9. September 10, 2025; partition2twomode, journals
+# version 10. June 6, 2026; OpenAlexInOut
 
 # TO DO:
 # Vector DC / http://localhost:8800/doku.php?id=work:bib:alex:ana:mat:clea
 # Work labels and titles / http://localhost:8800/doku.php?id=work:bib:alex:ana:mat:aci
+# call OpenAlexInOut in OpenAlexSources
 
 selPub  <- "id,primary_location,title,publication_year,cited_by_count,countries_distinct_count"
 selRef  <- "biblio,type,language,referenced_works_count,referenced_works"
@@ -545,7 +547,61 @@ authors <- function(L) {A <- L$authorships; k <- length(A); N <- rep("",k)
   for (i in 1:k) N[i] <- paste(A[i][[1]]$author$display_name,collapse=", ")
   return(N)
 }
-  
+
+OpenAlexInOut <- function(Wj,step=100,cond=""){
+  cat("OpenAlex2Pajek / in and out works",date(),"\n"); flush.console()
+  works <- "https://api.openalex.org/works"
+  nj <- nrow(Wj)
+  cat(nj,"seed works",date(),"\n"); flush.console()
+# --- Win ---
+  Win <- NULL; ni <- 0
+  for(k in 1:nj) if(Wj$cited_by_count[k]>0){
+    wID <- Wj$id[k]
+    Q <- list(filter=paste0("cites:",wID),
+      select="id,cited_by_count", per_page="200", cursor="*")
+    i <- 0
+    while(TRUE){
+      wd <- GET(works,query=Q)
+      if(wd$status_code!=200) break
+      wc <- fromJSON(rawToChar(wd$content))
+      Q$cursor <- wc$meta$next_cursor
+      if(is.null(Q$cursor)) break
+      df <- wc$results; nr <- nrow(df); ni <- ni+1
+      if(step>0) if(ni%%step==0) {cat(ifelse(ni%%(step*50)==0,".\n","."));
+        flush.console()}
+      # cat(k,nin,nr,"\n"); flush.console()
+      if(nr>0){df$id <- getID(df$id); Win <- rbind(Win,df)}
+    } 
+  }
+  cat("\n",nrow(Win),"citing works collected",date(),"\n"); flush.console()
+# --- Wout ---
+  Wout <- NULL; ri <- 0; no <- 0
+  while(TRUE) {
+    li <- ri+1; ri <- min(ri+50,nj)
+    if(li > ri) break
+    wID <- paste(Wj$id[li:ri],collapse="|")
+    Q <- list(filter=paste0("openalex:",wID),
+      select="id,referenced_works", per_page="200", page="1")
+    wd <- GET(works,query=Q)
+    if(wd$status_code!=200) next
+    wc <- fromJSON(rawToChar(wd$content))
+    df <- wc$results; nr <- nrow(df)
+    if(nr>0){ 
+      for(i in 1:nr){
+        Rid <- getID(unlist(df$referenced_works[i])); rn <- length(Rid); no <- no+1
+        if(step>0) if(no%%step==0) {cat(ifelse(no%%(step*50)==0,".\n","."));
+          flush.console()}
+        if(rn>0) Wout <- rbind(Wout,data.frame(id=Rid,cited_by_count=rep(-1,rn)))
+      }
+    } 
+  }
+  cat("\n",nrow(Wout),"cited works collected",date(),"\n"); flush.console()
+# --- combine ---
+  W <- union(Wout$id,union(Win$id,Wj$id))
+  cat(length(W),"different works",date(),"\n"); flush.console() 
+  return(W)
+}
+
 OpenAlexSources <- function(sID,step=100,cond=""){
   cat("OpenAlex2Pajek / Sources",date(),"\n"); flush.console()
   works <- "https://api.openalex.org/works"
